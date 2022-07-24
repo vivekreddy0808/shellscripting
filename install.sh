d427f35ecfa5b3cb9be3cf59315dd338b09a1721
vivek <vivekreddy0808@gmail.com>
2022-07-24 14:27:50 +0000

    added version

--- install.sh
+++ install.sh
@@ -4,4 +4,16 @@ echo " Check yum Module Updates "
 sudo yum update -y
 
 echo " Check version of Module "
-sudo yum --version 
+sudo yum --version
+
+echo -e  " install docker \n  "
+echo -e " Step 1: \t updating yum-utils"
+sudo yum install -y yum-utils
+
+echo -e " Step 2: \t add repo"
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+echo -e " Step 3: \t "
